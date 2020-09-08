3ed288cbd88653c002ae5a86848be66dd7ffcabc
Tarek Hammami <thammami.me@gmail.com>
2020-09-08 23:09:14 +0100

    Updates

---
+++ precache-manifest.4ee8594141864b97b8fc7acb994703f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ceba5f857ecc508790ce4bc7ac870c",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "ad2cdb06ef591266003e",
+    "url": "/React-App/static/css/main.efabe8db.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/React-App/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2cdb06ef591266003e",
+    "url": "/React-App/static/js/main.02ff29be.chunk.js"
+  },
+  {
+    "revision": "fab056ada119ad31482c",
+    "url": "/React-App/static/js/runtime-main.6d6e9462.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/React-App/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/React-App/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
